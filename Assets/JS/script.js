b974fdbf596b480d4ab006257069b3f396f7eae5
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2021-11-11 14:43:34 -0500

    Able to append input text and select dropdown values to task list

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,15 +1,36 @@
 //* Variables
 const buttonEl = document.querySelector("#save-task");
 const tasksToDoEl = document.querySelector("#tasks-to-do");
+const taskNameInput = document.querySelector("input[name='task-name']");
+const formEl = document.querySelector("#task-form");
+const taskTypeInput = document.querySelector("select[name='task-type']");
+console.log(taskTypeInput);
 
 //* Functions
 // Creates tasks
-const createTaskHandler = function () {
+const createTaskHandler = function (e) {
+  // Prevents site from reloading
+  e.preventDefault();
+  // Creates new list item
   const taskItemEl = document.createElement("li");
+  // Adds class to list item
   taskItemEl.classList.add("task-item");
-  taskItemEl.textContent = "New Task";
+  // Create div to hold task info
+  const taskInfoEl = document.createElement("div");
+  // Set its contents as the input and select values
+  taskInfoEl.innerHTML = `
+  <h3 class="task-name">
+    ${taskNameInput.value}
+  </h3> 
+  <span class="task-type">
+    ${taskTypeInput.value}
+  </span>
+  `;
+  // Append the div to the li
+  taskItemEl.appendChild(taskInfoEl);
+  // Append list item to ul
   tasksToDoEl.appendChild(taskItemEl);
 };
 
 //* Event Listeners
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
